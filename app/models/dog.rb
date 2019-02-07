ecce5921307ca0eceab002f83e1e55994cb49260
github username <github email address>
2019-02-07 17:40:12 -0500

    pairing lab

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,23 @@
 class Dog < ApplicationRecord
+  has_many :employees
+
+
+  def employee_count
+    self.employees.count
+  end
+
+
+  def employee_name=(name)
+    self.employee = Employee.find_or_create_by(first_name: name)
+  end
+
+  
+  def employee_name
+    self.employee ? self.employee.name : nil
+  end
+
+  # def employee_name=(name)
+  #   self.employee = Employee.find_or_create_by(first_name: first_name, last_name: last_name, alias: alias, title: title, office: office, img_url: img_url, dog_id: dog_id)
+  # end
+
 end
