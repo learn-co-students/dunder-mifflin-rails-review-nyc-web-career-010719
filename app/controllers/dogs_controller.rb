483d6c54f698ab0548f69bb44459c33414548481
github username <github email address>
2019-02-07 16:24:39 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,29 @@
 class DogsController < ApplicationController
+
+  def index
+    # byebug
+    @dogs = Dog.all
+
+    if params[:sort] == "1"
+      @dogs = @dogs.sort_by do |dog|
+        dog.employees.count
+      end
+    elsif params[:sort] == "2"
+      @dogs = @dogs.sort_by do |dog|
+        dog.employees.count
+      end
+      @dogs = @dogs.reverse
+    elsif params[:sort] == "3"
+      @dogs
+    end
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  private
+
+
+
 end
